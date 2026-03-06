1a644e6c45bd1a4bfa09056bc19e4ec126052803
origin <isjsjd0228@gmail.com>
2026-03-07 03:30:50 +1100

    created signup backend code (username, email, password) + unique check of email and username + password encryption

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,17 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import authRouter from "./routes/auth"
+import prisma from "./db";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const prisma = new PrismaClient({ adapter });
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/auth", authRouter)
 
 app.get("/", (req,res) => {
   res.send("Hello");
 })
-
-app.get("/recipes", async (req, res) => {
-  const recipes = await prisma.recipe.findMany();
-  res.json(recipes);
-});
 
 app.listen(4000, () => {
   console.log("Server running on http://localhost:4000")
